7495bbac3ed68f419cb4db2272d96f58d6eb81f6
Bobby Earl <bobby.earl@blackbaud.com>
2016-06-11 15:37:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-var remapIstanbul = require('remap-istanbul');
+module.exports = function(source, map) {
+  if (map) {
+    source += [
+      "\r\n",
+      "//# sourceMappingURL=data:application/json;base64,",
+      new Buffer(JSON.stringify(map)).toString('base64')
+    ].join('');
+  }
 
-var RemapReporter = function(helper, logger, config) {
-  var log = logger.create('reporter.remap');
-  var config = config || {};
-
-  var inputFile = config.inputFile || 'coverage-final.json';
-  var inputDir = config.inputDir || 'coverage/';
-  var outputDir = config.outputDir || 'coverage-remapped/';
-
-  this.onRunComplete = function(browsers, results) {
-    browsers.forEach(function (browser) {
-
-      log.info('Remapping coverage for ' + browser.name);
-      remapIstanbul(inputDir + browser.name + '/' + inputFile, {
-        'json': outputDir + browser.name + '/' + inputFile,
-        'html': outputDir + browser.name + '/'
-      });
-    })
-  };
-};
-
-RemapReporter.$inject = ['helper', 'logger','config.remapReporter'];
-
-module.exports = {
-  'reporter:remap': ['type', RemapReporter]
-};
+  return source;
+}
